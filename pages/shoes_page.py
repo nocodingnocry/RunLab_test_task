93dcefe32930bcf6b5f0285b7c17a71f41607eaa
Alexandr Luchkin <luchkin1997@mail.ru>
2022-11-23 09:12:43 +0300

    update shoes_page

--- pages/shoes_page.py
+++ pages/shoes_page.py
@@ -17,7 +17,9 @@ class Shoes_page():
     locator_slider_price = '//*[@id="filter-price-slider"]/span[1]'
     locator_slider_target = '/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div/div/div/form/div/div[3]/div[2]/div[1]/span[1]'
     locator_add_cart = '/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div/section/div/div[2]/div[1]/div[3]/div[4]/a'
-    locators_shows = '//*[@id="filter-tooltip-button"]'
+    locator_show = '//*[@id="filter-tooltip-button"]'
+    locator_size = '//*[@id="pre-buy-size-choice-popup"]'
+    locator_move_to_cart = '//*[@id="move-to-basket-popup"]'
 
     #Getters
 
@@ -34,32 +36,47 @@ class Shoes_page():
         return WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, self.locator_hoka)))
 
     def get_shows(self):
-        return WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, self.locators_shows)))
+        return WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, self.locator_show)))
 
-    def target(self):
-        return WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, self.locator_slider_target)))
+    def get_size(self):
+        return WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, self.locator_size)))
+
+    def get_move_to_cart(self):
+        return WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, self.locator_move_to_cart)))
+
     #Actions
 
     def choice_brand(self):
         self.get_brands_list().click()
         self.get_hoka().click()
     def change_price_on_slider(self):
-        action = ActionChains(self)
-        loc = self.get_slider_price()
-        tar = self.target()
-        action.drag_and_drop(loc, tar).perform()
+        action = ActionChains(self.driver)
+        action.drag_and_drop_by_offset(self.get_slider_price(), 20, 0)
+        action.perform()
         print("Change price on slider is completed")
+        self.get_shows().click()
 
     def click_add_cart(self):
-        self.get_add_cart().click()
+        action = ActionChains(self.driver)
+        #action.move_to_element(self.get_add_cart()).click()
+        self.move_to_cart(self.get_add_cart()).perform()
+
         print("Clicked to add shoes in cart")
+
+    def click_on_size(self):
+        self.get_size().click()
+
+    def move_to_cart(self):
+        self.get_move_to_cart().perform()
+        self.get_move_to_cart().click()
 
     #Methods
     def setting_filtres_and_add_product(self):
         self.choice_brand()
-        time.sleep(5)
         self.change_price_on_slider()
         self.click_add_cart()
+        self.get_size()
+        self.move_to_cart()
 
 
 
